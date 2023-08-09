6f5dfcb78a57626c8031afbe0ee017b3c2ce2dab
debart-nathan <denathan@laposte.net>
2023-08-09 11:31:11 +0200

    generate footer

--- ex/tp/templates/footer.php
+++ ex/tp/templates/footer.php
@@ -1,3 +1,7 @@
-<footer >
-    test
+<footer class="bg-dark p-3 text-white">
+    <div class="d-flex justify-content-evenly">
+        <p class="my-1">Association la belle affaire</p>
+        <p class="my-1">2 Avenue des champs Elysées</p>
+        <p class="my-1">75000 Paris</p>
+    </div>
 </footer>
